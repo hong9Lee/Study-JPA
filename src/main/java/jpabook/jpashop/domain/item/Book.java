a73ee760c54b4cab97de21e595b16ecf7da94132
hong9Lee <hongtemp9mac@gmail.com>
2022-07-12 18:55:43 +0900

    first commit

---
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("B")
+@Getter
+@Setter
+public class Book extends Item{
+    private String author;
+    private String isbn;
+}
